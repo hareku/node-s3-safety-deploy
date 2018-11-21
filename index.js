da79050934aa6769658b23449ca9dbdeb36445ee
hareku <hareku908@gmail.com>
2018-11-21 21:46:04 +0900

    improve cli

--- index.js
+++ index.js
@@ -20,105 +20,103 @@ const argv = parseArgs(process.argv.slice(2), {
   string: ['b', 'bd', 'ud', 'dp']
 })
 
-const main = async () => {
-  const S3 = new AWS.S3()
-  const { bucket, bucketDir, uploadDir, deletePattern } = argv
+module.exports = async () => {
+  try {
+    const S3 = new AWS.S3()
+    const { bucket, bucketDir, uploadDir, deletePattern } = argv
 
-  logger.info(`Bucket: ${bucket}`)
-  logger.info(`Bucket Directory: ${bucketDir}`)
-  logger.info(`Upload Directory: ${uploadDir}`)
+    logger.info(`Bucket: ${bucket}`)
+    logger.info(`Bucket Directory: ${bucketDir}`)
+    logger.info(`Upload Directory: ${uploadDir}`)
 
-  const { Contents } = await S3.listObjectsV2({
-      Bucket: bucket,
-      Prefix: bucketDir || null
-    }).promise()
-  const beforeUploadObjects = Contents
-
-  /**
-   * Upload
-   */
-  const uploadFiles = walkSync(uploadDir, { directories: false })
-  const uploadS3Keys = []
-  logger.info(`Uploading new ${uploadFiles.length} files`)
-
-  for (uploadFile of uploadFiles) {
-    const file = await promisify(fs.readFile)(`${trailingSlashIt(uploadDir)}${uploadFile}`)
-    const s3key = bucketDir
-      ? `${trailingSlashIt(bucketDir)}${uploadFile}`
-      : uploadFile
-
-    uploadS3Keys.push(s3key)
-    logger.info(s3key)
-
-    await S3.putObject({
-      Bucket: bucket,
-      Key: s3key,
-      Body: file
-    }).promise()
-  }
-
-  logger.success(`Uploaded new ${uploadFiles.length} files`)
-
-  /**
-   * Delete
-   */
-  logger.info(`deletePattern is ${deletePattern}`)
-  const deleteObjects = getDeleteObjects(beforeUploadObjects, uploadS3Keys, deletePattern)
-
-  if (deleteObjects.length === 0) {
-    logger.info('No objects to delete')
-  } else {
-    logger.info(`Deleting ${deleteObjects.length} files`)
-
-    await S3.deleteObjects({
-      Bucket: bucket,
-      Delete: {
-        Objects: deleteObjects.map(object => {
-          logger.info(object.Key)
-          return { Key: object.Key }
-        })
-      }
-    }).promise()
-
-    logger.info(`Deleted ${deleteObjects.length} files`)
-  }
-
-  /**
-   * Tag
-   */
-  const tagObjects = getTagObjects(beforeUploadObjects, uploadS3Keys, deleteObjects)
-
-  if (tagObjects.length === 0) {
-    logger.info('No objects to tag')
-  } else {
-    logger.info(`Tagging ${tagObjects.length} old files`)
-
-    for (tagObject of tagObjects) {
-      logger.info(tagObject.Key)
-
-      await S3.putObjectTagging({
+    const { Contents } = await S3.listObjectsV2({
         Bucket: bucket,
-        Key: tagObject.Key,
-        Tagging: {
-          TagSet: [
-            {
-              Key: 'ShouldDelete',
-              Value: "1"
-            }
-          ]
-        }
+        Prefix: bucketDir || null
+      }).promise()
+    const beforeUploadObjects = Contents
+
+    /**
+     * Upload
+     */
+    const uploadFiles = walkSync(uploadDir, { directories: false })
+    const uploadS3Keys = []
+    logger.info(`Uploading new ${uploadFiles.length} files`)
+
+    for (uploadFile of uploadFiles) {
+      const file = await promisify(fs.readFile)(`${trailingSlashIt(uploadDir)}${uploadFile}`)
+      const s3key = bucketDir
+        ? `${trailingSlashIt(bucketDir)}${uploadFile}`
+        : uploadFile
+
+      uploadS3Keys.push(s3key)
+      logger.info(s3key)
+
+      await S3.putObject({
+        Bucket: bucket,
+        Key: s3key,
+        Body: file
       }).promise()
     }
 
-    logger.info(`Tagged ${tagObjects.length} old files`)
+    logger.success(`Uploaded new ${uploadFiles.length} files`)
+
+    /**
+     * Delete
+     */
+    logger.info(`deletePattern is ${deletePattern}`)
+    const deleteObjects = getDeleteObjects(beforeUploadObjects, uploadS3Keys, deletePattern)
+
+    if (deleteObjects.length === 0) {
+      logger.info('No objects to delete')
+    } else {
+      logger.info(`Deleting ${deleteObjects.length} files`)
+
+      await S3.deleteObjects({
+        Bucket: bucket,
+        Delete: {
+          Objects: deleteObjects.map(object => {
+            logger.info(object.Key)
+            return { Key: object.Key }
+          })
+        }
+      }).promise()
+
+      logger.info(`Deleted ${deleteObjects.length} files`)
+    }
+
+    /**
+     * Tag
+     */
+    const tagObjects = getTagObjects(beforeUploadObjects, uploadS3Keys, deleteObjects)
+
+    if (tagObjects.length === 0) {
+      logger.info('No objects to tag')
+    } else {
+      logger.info(`Tagging ${tagObjects.length} old files`)
+
+      for (tagObject of tagObjects) {
+        logger.info(tagObject.Key)
+
+        await S3.putObjectTagging({
+          Bucket: bucket,
+          Key: tagObject.Key,
+          Tagging: {
+            TagSet: [
+              {
+                Key: 'ShouldDelete',
+                Value: "1"
+              }
+            ]
+          }
+        }).promise()
+      }
+
+      logger.info(`Tagged ${tagObjects.length} old files`)
+    }
+
+    logger.success('Successfully deployment')
+  } catch (e) {
+    logger.error(e)
+    process.exit(1)
   }
-
-  logger.success('Successfully deployment')
-}
-
-try {
-  main()
-} catch (e) {
-  logger.error(e)
-  process.exit(1)
 }
